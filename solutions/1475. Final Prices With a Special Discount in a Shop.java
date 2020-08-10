83e45e41ad8a923de45abe77f8d934f7a9125c59
Akhil Madham Shetty <akhilmadhamshetty21@gmail.com>
2020-08-10 17:46:51 -0400

    1 ms

--- solutions/1475. Final Prices With a Special Discount in a Shop.java
+++ solutions/1475. Final Prices With a Special Discount in a Shop.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        // List<Integer> result=new ArrayList<Integer>();
+   
         int[] result=new int[prices.length];
         for(int i=0;i<prices.length-1;i++){
             for(int j=i+1;j<prices.length;j++){
@@ -9,7 +9,7 @@ class Solution {
                     break;
                 }
                 result[i]=prices[i];
-            }
+            }  
         }
         result[prices.length-1]=prices[prices.length-1];
         return result;
